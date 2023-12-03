fc552def149c3e1fd644f7b2396a614b58adbbbd
Bayan2019 <saparbayevabt@gmail.com>
2023-12-03 02:53:36 -0800

    adding function and tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,2 +1,30 @@
 const { test, expect } = require('@jest/globals')
 const { normalizeURL } = require('./crawl.js')
+
+test('normalizedURL strip protocol', () => {
+    const input = 'https://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizedURL strip trailing slash', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizedURL strip capital', () => {
+    const input = 'https://BLOG.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizedURL strip http', () => {
+    const input = 'http://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
